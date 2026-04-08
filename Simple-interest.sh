6507a8cbae624c31b85ee257bb47303e7b9a9bcd
kellyannjoefield24-source <kellyannjoefield24@gmail.com>
2026-04-08 07:06:21 -0300

    Create Simple-interest.sh

---
+++ Simple-interest.sh
@@ -0,0 +1,1 @@
+https://github.com/kellyannjoefield24-source/github-final-project/blob/main/simple-interest.sh%5D(https://github.com/kellyannjoefield24-source/github-final-project/blob/main/simple-interest.sh)
